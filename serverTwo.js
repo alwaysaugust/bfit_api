0a4c93599784fb752a2edcfefefb42323b0f118f
Oleksiy Martynov <oleksiy@alwaysaugust.com>
2019-10-25 21:30:37 -0400

    followup

--- serverTwo.js
+++ serverTwo.js
@@ -1,5 +1,5 @@
 const PORT = process.env.PORT || 5000;
-const PUBLIC_ADDRESS = process.env.PUBLIC_ADDRESS || "http://localhost";
+const PUBLIC_ADDRESS = process.env.PUBLIC_ADDRESS || "http://localhost:5000";
 const FRONTEND = process.env.FRONTEND || "http://localhost:3000";
 // Required dependencies
 const express = require("express");
@@ -44,7 +44,7 @@ passport.use(
       clientID:
         "658254840670-23f7m3u1hpo8popp3ovsscu3r8qipap2.apps.googleusercontent.com",
       clientSecret: "BcbAks57gpAYRaLQaXU_6Qk9",
-      callbackURL: `${PUBLIC_ADDRESS}:${PORT}/auth/google/callback`
+      callbackURL: `${PUBLIC_ADDRESS}/auth/google/callback`
     },
     (accessToken, refreshToken, profile, cb) => {
       console.log({ accessToken, refreshToken });
